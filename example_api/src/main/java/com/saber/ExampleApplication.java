5b4478121e3f4eae53c9ea8feaceb2f2a36f4a30
saber <leo.sgr@foxmail.com>
2018-03-01 19:06:36 +0800

    spring session

--- example_api/src/main/java/com/saber/ExampleApplication.java
+++ example_api/src/main/java/com/saber/ExampleApplication.java
@@ -2,6 +2,7 @@ package com.saber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * @author saber
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create_time 2018/2/23 上午11:40
  */
 @SpringBootApplication
+@EnableRedisHttpSession
 public class ExampleApplication {
 
     public static void main(String[] args) {
